62bd4b8fffd0ff447116ee786b6fffcb920e4945
Lucas-Kurth <lmjkurth@icloud.com>
2025-12-30 00:00:46 +0100

    Delete Luca_Kurth_CSIA directory

--- Luca_Kurth_CSIA/src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-
-
-public class Main {
-    public static void main(String[] args)
-    {
-        iDandPass idandpass = new iDandPass();
-        LoginPage loginPage = new LoginPage(iDandPass.getLoginInfo());
-    }
-}
